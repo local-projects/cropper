cd7450c718283f91b36e3d732df6eddfb6db9521
adarshk <adarshkosuru@localprojects.net>
2016-08-03 11:34:14 -0400

    spacing

--- demo/js/Skeleton.js
+++ demo/js/Skeleton.js
@@ -172,224 +172,224 @@ Skeleton.prototype = {
 	},
 
 	getDefaults: function () {
-    var containerWid = 375, containerHt = 500;
-    var xVal = (containerWid - 10) / 2;
-    var yVal = 10;
-    var shoulderPos = 50;
-    var torsoHt = 200;
-    
-    var drawOptions = {
-      container: {
-        width: containerWid, 
-        height: containerHt
-      },
+		var containerWid = 375, containerHt = 500;
+		var xVal = (containerWid - 10) / 2;
+		var yVal = 10;
+		var shoulderPos = 50;
+		var torsoHt = 200;
 
-      common: {
-        width: 10,
-        fill: 'lightgrey'
-      },
+		var drawOptions = {
+			container: {
+				width: containerWid, 
+				height: containerHt
+			},
 
-      torso : {
-        x: xVal, 
-        y: yVal, 
-        height: torsoHt, 
-        id: 'torso-body',
-        partOf: 'body',
-        pivot: {
-          x: xVal - 5,
-          y: yVal - 5,
-          id: 'torso',
-          class: 'pivot'
-        }
-      },
+			common: {
+				width: 10,
+				fill: 'lightgrey'
+			},
 
-      leftShoulder: {
-        x: xVal, 
-        y: shoulderPos, 
-        height: 60, 
-        style: 'transform: rotate(70deg)', 
-        class: 'left-hand', 
-        id: 'left-shoulder',
-        partOf: 'hand'
-      },
+			torso : {
+				x: xVal, 
+				y: yVal, 
+				height: torsoHt, 
+				id: 'torso-body',
+				partOf: 'body',
+				pivot: {
+					x: xVal - 5,
+					y: yVal - 5,
+					id: 'torso',
+					class: 'pivot'
+				}
+			},
 
-      rightShoulder: {
-        x: xVal, 
-        y: shoulderPos, 
-        height: 60, 
-        style: 'transform: rotate(-70deg)', 
-        class: 'right-hand', 
-        id: 'right-shoulder',
-        partOf: 'hand'
-      },
+			leftShoulder: {
+				x: xVal, 
+				y: shoulderPos, 
+				height: 60, 
+				style: 'transform: rotate(70deg)', 
+				class: 'left-hand', 
+				id: 'left-shoulder',
+				partOf: 'hand'
+			},
 
-      leftElbow: {
-        x: xVal - 57.5, 
-        /*x: 125, */
-        y: shoulderPos + 20, 
-        height: 50, 
-        style: 'transform:rotate(45deg)', 
-        class: 'left-hand', 
-        id: 'left-elbow',
-        partOf: 'hand',
-        pivot: {
-          x: xVal - 67.5,
-          y: shoulderPos + 15,
-          id: 'pivot-elbow-left',
-          class: 'pivot'
-        }
-      },
+			rightShoulder: {
+				x: xVal, 
+				y: shoulderPos, 
+				height: 60, 
+				style: 'transform: rotate(-70deg)', 
+				class: 'right-hand', 
+				id: 'right-shoulder',
+				partOf: 'hand'
+			},
 
-      rightElbow: {
-        x: xVal + 57.5, 
-        /*x: 240,*/
-        y: shoulderPos + 20, 
-        height: 50, 
-        style: 'transform:rotate(-45deg)', 
-        class: 'right-hand', 
-        id: 'right-elbow',
-        partOf: 'hand',
-        pivot: {
-          x: xVal + 57.5,
-          y: shoulderPos + 15,
-          id: 'pivot-elbow-right',
-          class: 'pivot'
-        }
-      },
+			leftElbow: {
+				x: xVal - 57.5, 
+				/*x: 125, */
+				y: shoulderPos + 20, 
+				height: 50, 
+				style: 'transform:rotate(45deg)', 
+				class: 'left-hand', 
+				id: 'left-elbow',
+				partOf: 'hand',
+				pivot: {
+					x: xVal - 67.5,
+					y: shoulderPos + 15,
+					id: 'pivot-elbow-left',
+					class: 'pivot'
+				}
+			},
 
-      leftHand: {
-        x: xVal - 45 - 47, 
-        y: shoulderPos + 55, 
-        height: 50, 
-        style: 'transform:rotate(35deg)', 
-        class: 'left-hand', 
-        id: 'left-hand',
-        partOf: 'hand',
-        pivot: {
-          x: xVal - 45 - 47 - 10,
-          y: shoulderPos + 55,
-          id: 'pivot-hand-left',
-          class: 'pivot'
-        }
-      },
+			rightElbow: {
+				x: xVal + 57.5, 
+				/*x: 240,*/
+				y: shoulderPos + 20, 
+				height: 50, 
+				style: 'transform:rotate(-45deg)', 
+				class: 'right-hand', 
+				id: 'right-elbow',
+				partOf: 'hand',
+				pivot: {
+					x: xVal + 57.5,
+					y: shoulderPos + 15,
+					id: 'pivot-elbow-right',
+					class: 'pivot'
+				}
+			},
 
-      rightHand: {
-        x: xVal + 45 + 47, 
-        y: shoulderPos + 55, 
-        height: 50, 
-        style: 'transform:rotate(-35deg)', 
-        class: 'right-hand', 
-        id: 'right-hand',
-        partOf: 'hand',
-        pivot: {
-          x: xVal + 45 + 47,
-          y: shoulderPos + 55,
-          id: 'pivot-hand-right',
-          class: 'pivot'
-        }
-      },
+			leftHand: {
+				x: xVal - 45 - 47, 
+				y: shoulderPos + 55, 
+				height: 50, 
+				style: 'transform:rotate(35deg)', 
+				class: 'left-hand', 
+				id: 'left-hand',
+				partOf: 'hand',
+				pivot: {
+					x: xVal - 45 - 47 - 10,
+					y: shoulderPos + 55,
+					id: 'pivot-hand-left',
+					class: 'pivot'
+				}
+			},
 
-      leftPalm: {
-        x: xVal - 45 - 47 - 25, 
-        y: shoulderPos + 90, 
-        height: 25, 
-        style: 'transform:rotate(45deg)', 
-        class: 'left-hand', 
-        id: 'left-palm',
-        partOf: 'hand'
-      },
+			rightHand: {
+				x: xVal + 45 + 47, 
+				y: shoulderPos + 55, 
+				height: 50, 
+				style: 'transform:rotate(-35deg)', 
+				class: 'right-hand', 
+				id: 'right-hand',
+				partOf: 'hand',
+				pivot: {
+					x: xVal + 45 + 47,
+					y: shoulderPos + 55,
+					id: 'pivot-hand-right',
+					class: 'pivot'
+				}
+			},
 
-      rightPalm: {
-        x: xVal + 45 + 47 + 25, 
-        y: shoulderPos + 90, 
-        height: 25, 
-        style: 'transform:rotate(-45deg)', 
-        class: 'right-hand', 
-        id: 'right-palm',
-        partOf: 'hand'
-      },
+			leftPalm: {
+				x: xVal - 45 - 47 - 25, 
+				y: shoulderPos + 90, 
+				height: 25, 
+				style: 'transform:rotate(45deg)', 
+				class: 'left-hand', 
+				id: 'left-palm',
+				partOf: 'hand'
+			},
 
-      leftHip: {
-        x: xVal, 
-        y: torsoHt, 
-        height: 25, 
-        style: 'transform:rotate(60deg)', 
-        class: 'left-leg', 
-        id: 'left-hip',
-        partOf: 'leg'
-      },
+			rightPalm: {
+				x: xVal + 45 + 47 + 25, 
+				y: shoulderPos + 90, 
+				height: 25, 
+				style: 'transform:rotate(-45deg)', 
+				class: 'right-hand', 
+				id: 'right-palm',
+				partOf: 'hand'
+			},
 
-      rightHip: {
-        x: xVal, 
-        y: torsoHt, 
-        height: 25, 
-        style: 'transform:rotate(-60deg)', 
-        class: 'right-leg', 
-        id: 'right-hip',
-        partOf: 'leg'
-      },
+			leftHip: {
+				x: xVal, 
+				y: torsoHt, 
+				height: 25, 
+				style: 'transform:rotate(60deg)', 
+				class: 'left-leg', 
+				id: 'left-hip',
+				partOf: 'leg'
+			},
 
-      leftKnee: {
-        x: xVal - 20, 
-        y: torsoHt + 10, 
-        height: 125, 
-        style: 'transform:rotate(15deg)', 
-        class: 'left-leg', 
-        id: 'left-knee',
-        partOf: 'leg'
-      },
+			rightHip: {
+				x: xVal, 
+				y: torsoHt, 
+				height: 25, 
+				style: 'transform:rotate(-60deg)', 
+				class: 'right-leg', 
+				id: 'right-hip',
+				partOf: 'leg'
+			},
 
-      rightKnee: {
-        x: xVal + 20, 
-        y: torsoHt + 10, 
-        height: 125, 
-        style: 'transform:rotate(-15deg)', 
-        class: 'right-leg', 
-        id: 'right-knee',
-        partOf: 'leg'
-      },
+			leftKnee: {
+				x: xVal - 20, 
+				y: torsoHt + 10, 
+				height: 125, 
+				style: 'transform:rotate(15deg)', 
+				class: 'left-leg', 
+				id: 'left-knee',
+				partOf: 'leg'
+			},
 
-      leftKnee2: {
-        x: xVal - 52, 
-        y: torsoHt + 25 + 105, 
-        height: 75, 
-        style: 'transform:rotate(0deg)', 
-        class: 'left-leg', 
-        id: 'left-knee2',
-        partOf: 'leg'
-      },
+			rightKnee: {
+				x: xVal + 20, 
+				y: torsoHt + 10, 
+				height: 125, 
+				style: 'transform:rotate(-15deg)', 
+				class: 'right-leg', 
+				id: 'right-knee',
+				partOf: 'leg'
+			},
 
-      rightKnee2: {
-        x: xVal + 52, 
-        y: torsoHt + 25 + 105, 
-        height: 75, 
-        style: 'transform:rotate(0deg)', 
-        class: 'right-leg', 
-        id: 'right-knee2',
-        partOf: 'leg'
-      },
+			leftKnee2: {
+				x: xVal - 52, 
+				y: torsoHt + 25 + 105, 
+				height: 75, 
+				style: 'transform:rotate(0deg)', 
+				class: 'left-leg', 
+				id: 'left-knee2',
+				partOf: 'leg'
+			},
 
-      leftFoot: {
-        x: xVal - 52, 
-        y: torsoHt + 25 + 105 + 70, 
-        height: 25, 
-        style: 'transform:rotate(45deg)', 
-        class: 'left-leg', 
-        id: 'left-foot',
-        partOf: 'leg'
-      },
+			rightKnee2: {
+				x: xVal + 52, 
+				y: torsoHt + 25 + 105, 
+				height: 75, 
+				style: 'transform:rotate(0deg)', 
+				class: 'right-leg', 
+				id: 'right-knee2',
+				partOf: 'leg'
+			},
 
-      rightFoot: {
-        x: xVal + 52, 
-        y: torsoHt + 25 + 105 + 70, 
-        height: 25, 
-        style: 'transform:rotate(-45deg)', 
-        class: 'right-leg', 
-        id: 'right-foot',
-        partOf: 'leg'
-      }
-    }
+			leftFoot: {
+				x: xVal - 52, 
+				y: torsoHt + 25 + 105 + 70, 
+				height: 25, 
+				style: 'transform:rotate(45deg)', 
+				class: 'left-leg', 
+				id: 'left-foot',
+				partOf: 'leg'
+			},
 
-    return drawOptions;
-  },
+			rightFoot: {
+				x: xVal + 52, 
+				y: torsoHt + 25 + 105 + 70, 
+				height: 25, 
+				style: 'transform:rotate(-45deg)', 
+				class: 'right-leg', 
+				id: 'right-foot',
+				partOf: 'leg'
+			}
+		}
+
+		return drawOptions;
+	},
 }
